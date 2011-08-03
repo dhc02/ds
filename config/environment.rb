4633a0518f2c3b3c134f31b7e73551ac1ff795e8
Donnie Clapp <github@thedcdesign.com>
2011-08-03 12:10:23 -0700

    trying some stuff

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV['GEM_HOME']="/home/dhc02/.rvm/gems/ruby-1.9.2-p290"
+EVN['GEM_PATH']="/home/dhc02/.rvm/gems/ruby-1.9.2-p290:/home/dhc02/.rvm/gems/ruby-1.9.2-p290@global"
+
 # Initialize the rails application
 DammitsteveCom::Application.initialize!
