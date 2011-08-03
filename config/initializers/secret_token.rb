dd20c93890c5d7380f13b6fffe2cd5d00ab8fc56
donnie.clapp <donnieclapp@dcla.mercurycsc.localhost>
2011-08-03 12:19:19 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DammitsteveCom::Application.config.secret_token = 'ea914679e6685a413d8a79b6258905c58a329250bbb246581cad4e6072ee5133ec218f1f9a988d523a97aad563509d9d9a5a029027af1f6d49ae45f2750bd4b1'
